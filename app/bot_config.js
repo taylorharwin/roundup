64c491854746e653b95426d95f05bb2f2ec8a6d0
Taylor Harwin <taylor.harwin@gmail.com>
2016-04-15 18:08:56 -0700

    Delete bot_config.js

--- app/bot_config.js
+++
@@ -1,9 +0,0 @@
-var SlackBot = require('slackbots');
- 
-// create a bot 
-var bot = new SlackBot({
-    token: 'xoxb-22438458181-8gbDg17qS9e4kP8EhrdRkFGK', // Add a bot https://my.slack.com/services/new/bot and put the token  
-    name: 'Roundup'
-});
- 
-module.exports = bot;
